ed7353582fefae798d406e315b5cc5679584c522
Jon Frisby <jfrisby@mrjoy.com>
2015-07-19 23:51:49 -0700

    Add a home for HTTP code.

---
+++ lib/flux_hue/http.rb
@@ -0,0 +1,7 @@
+module FluxHue
+  # Helpers for Hue Bridge REST requests.  Placeholder to accrue redundant code
+  # in order to DRY things up, centralize response handling, and prepare for
+  # a more streamlined approach to interacting with the bridge.
+  class HTTP
+  end
+end
