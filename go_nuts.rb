f13aed963fc2659f95740fbede2415c54250d9e5
Jon Frisby <jfrisby@mrjoy.com>
2015-07-20 10:50:08 -0700

    First pass at trying to use Curb.

--- go_nuts.rb
+++ go_nuts.rb
@@ -3,12 +3,14 @@ require "rubygems"
 require "bundler/setup"
 Bundler.setup
 require "curb"
+require "oj"
 
 ###############################################################################
 # Timing Configuration
 #
 # Play with this to see how error rates are affected.
 ###############################################################################
+THREAD_COUNT    = 4
 ITERATIONS      = 20
 SPREAD_SLEEP    = 0.007
 TOTAL_SLEEP     = 0.1
@@ -36,52 +38,78 @@ def random_hue; rand(16) * 4096; end
 
 ###############################################################################
 ###############################################################################
+def hue_base
+  "http://#{BRIDGE_IP}"
+end
+
 def hue_endpoint(light_id)
-  "http://#{BRIDGE_IP}/api/#{USERNAME}/lights/#{light_id}/state"
+  "#{hue_base}/api/#{USERNAME}/lights/#{light_id}/state"
 end
 
-def hue_request(hue, transition)
-  %({"hue":#{hue},"transitiontime":#{(transition * 10.0).round(0)}})
+def hue_request(light_id, hue, transition)
+  data = { "hue"            => hue,
+           "transitiontime" => (transition * 10.0).round(0) }
+
+  { light_id:   light_id,
+    url:        hue_endpoint(light_id),
+    put_data:   Oj.dump(data) }
 end
 
-def guard_call(light_id, &block)
+def guard_call(thread_idx, &block)
   block.call
-rescue StandardError => e
-  puts "Exception for light #{light_id}, got:"
+rescue Exception => e
+  puts "Exception for thread ##{thread_idx}, got:"
   puts "\t#{e.message}"
   puts "\t#{e.backtrace.join("\n\t")}"
 end
 
 puts "Mucking with #{LIGHTS.length} lights..."
 
+MULTI_OPTIONS = { pipeline: true }
+
+lights_for_threads = (1..THREAD_COUNT).map { [] }
+idx = 0
+LIGHTS.each do |light_id|
+  lights_for_threads[idx] << light_id
+  idx  += 1
+  idx   = 0 if idx >= THREAD_COUNT
+end
+
 successes = 0
 failures  = 0
-threads   = LIGHTS.map do |light_id|
+
+threads = (1..THREAD_COUNT).map do |thread_idx|
   sleep SPREAD_SLEEP
   Thread.new do
-    guard_call(light_id) do
-      counter = 0
-      while counter < 20
+    guard_call(thread_idx) do
+      agent           = Curl::Multi.new
+      agent.pipeline  = true
+      counter         = 0
+      while counter < ITERATIONS
         counter += 1
-        new_hue = random_hue
-        result = system %(
-          curl --tcp-nodelay \
-            --request PUT \
-            --silent \
-            -d '#{hue_request(new_hue, TRANSITION_TIME)}' \
-            '#{hue_endpoint(light_id)}' >/dev/null 2>&1
-        )
-        if result
-          successes += 1
-        else
-          failures += 1
-        end
+        requests  = LIGHTS
+                    .map { |lid| hue_request(lid, random_hue, TRANSITION_TIME) }
+                    .map do |req|
+                      Curl::Easy.new(req[:url]) do |curl|
+                        curl.put_data = req[:put_data]
+                        # curl.on_body do |data|
+                        #   puts "For URL: #{req[:url]}\n\t#{data}"
+                        # end
+                        curl.on_success { |_easy| successes += 1 }
+                        curl.on_failure do |easy|
+                          puts "ERROR for light ##{req[:light_id]} on"\
+                            " thread ##{thread_idx}: #{easy.inspect}"
+                          failures += 1
+                        end
+                      end
+                    end
+        requests.each { |req| agent.add(req) }
+        agent.perform
+
         sleep(FIXED_SLEEP + rand(VARIABLE_SLEEP))
-        # | grep -v success
-        # sleep rand(0.075)+0.025
       end
     end
-    puts "Finishing thread for light ##{light_id}."
+    puts "Finishing thread ##{thread_idx}."
   end
 end
 
@@ -94,4 +122,4 @@ puts "Done."
 puts "* #{requests} requests"
 puts "* #{successes} successful"
 puts "* #{failures} failed"
-puts "* #{"%0.2f" % ((failures / requests.to_f) * 100)}% failure rate"
+puts "* #{'%0.2f' % ((failures / requests.to_f) * 100)}% failure rate"
