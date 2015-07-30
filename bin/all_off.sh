c554029c6a60d0da1e4aa0bd814f5c8389695676
Jon Frisby <jfrisby@mrjoy.com>
2015-07-30 10:03:02 -0700

    Handy helper scripts.

---
+++ bin/all_off.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+HUE_BRIDGE_IP=192.168.2.8 bin/hue groups set 0 --state=off
+HUE_BRIDGE_IP=192.168.2.45 bin/hue groups set 0 --state=off
+HUE_BRIDGE_IP=192.168.2.46 bin/hue groups set 0 --state=off
+HUE_BRIDGE_IP=192.168.2.51 bin/hue groups set 0 --state=off
