b7bb10ed8c0de15730814c2015bab20efd196837
Jon Frisby <jfrisby@mrjoy.com>
2015-07-30 11:55:56 -0700

    Make this a bit cleaner for multi-process output.

--- bin/watch_memory.sh
+++ bin/watch_memory.sh
@@ -4,6 +4,7 @@ while [ 1 ]; do
   ps auxwww |
     grep rub[y] |
     grep go_nuts |
-    awk '{ print $6 }'
+    awk '{ print $6 }' | perl -pse 's/\n/, /g' | perl -pse 's/, $//'
+    echo
   sleep 1
 done
