02f3a667a2272ad29a62af1e031053b491c03f05
Jon Frisby <jfrisby@mrjoy.com>
2015-07-30 12:39:08 -0700

    Be better behaved when no processes are running.

--- bin/watch_memory.sh
+++ bin/watch_memory.sh
@@ -4,7 +4,6 @@ while [ 1 ]; do
   ps auxwww |
     grep rub[y] |
     grep go_nuts |
-    awk '{ print $6 }' | perl -pse 's/\n/, /g' | perl -pse 's/, $//'
-    echo
+    awk '{ print $6 }' | perl -pse 's/\n/, /g' | perl -pse 's/, $/\n/'
   sleep 1
 done
