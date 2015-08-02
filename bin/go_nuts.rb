55d70fad78d20ad27a35b9a1acac8f219b539e11
Jon Frisby <jfrisby@mrjoy.com>
2015-08-01 19:22:41 -0700

    Clean up stats code.

--- bin/go_nuts.rb
+++ bin/go_nuts.rb
@@ -431,17 +431,29 @@ debug "Threads are ready to go, waking them up."
 global_results.begin!
 threads.each(&:run)
 
-
 def format_float(num); num ? num.round(2) : "-"; end
+
+def format_rate(rate); "#{format_float(rate)}/sec"; end
+
+def print_stat(name, value, rate)
+  important "* #{value} #{name} (#{format_rate(rate)})"
+end
+
+STATS = [
+  ["requests",       :requests,      :requests_sec],
+  ["successes",      :successes,     :successes_sec],
+  ["failures",       :failures,      :failures_sec],
+  ["hard timeouts",  :hard_timeouts, :hard_timeouts_sec],
+  ["soft timeouts",  :soft_timeouts, :soft_timeouts_sec],
+]
 
 # TODO: Show per-bridge and aggregate stats.
 def print_results(results)
   important ""
-  important "* #{results.requests} requests (#{format_float(results.requests_sec)}/sec)"
-  important "* #{results.successes} successful (#{format_float(results.successes_sec)}/sec)"
-  important "* #{results.failures} failed (#{format_float(results.failures_sec)}/sec)"
-  important "* #{results.hard_timeouts} hard timeouts (#{format_float(results.hard_timeouts_sec)}/sec)"
-  important "* #{results.soft_timeouts} soft timeouts (#{format_float(results.soft_timeouts_sec)}/sec)"
+  STATS.each do |(name, count, rate)|
+    print_stat(name, results.send(count), results.send(rate))
+  end
+
   important "* #{format_float(results.failure_rate)}% failure rate"
   suffix = " (#{format_float(results.elapsed / ITERATIONS.to_f)}/iteration)" if ITERATIONS > 0
   important "* #{format_float(results.elapsed)} seconds elapsed#{suffix}"
