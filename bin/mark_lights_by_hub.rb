1dbbe0e3dd69c81ac4227f5c93e4fac0ee36d95b
Jon Frisby <jfrisby@mrjoy.com>
2015-08-03 23:29:08 -0700

    Make this a bit more visually clear, I hope.

--- bin/mark_lights_by_hub.rb
+++ bin/mark_lights_by_hub.rb
@@ -22,10 +22,13 @@ in_groups(CONFIG["main_lights"]).map do |(bridge_name, lights)|
   requests  = lights
               .map do |(idx, lid)|
                 LazyRequestConfig.new(config, hue_light_endpoint(config, lid)) do
+                  target = (254 * ((idx + 1) / lights.length.to_f)).round
                   data = {}
+                  data["on"] = true
                   data["hue"] = config["debug_hue"]
-                  data["sat"] = ((200 * (idx / lights.length.to_f)) + 54).round
-                  data["bri"] = (254 * (idx / lights.length.to_f)).round
+                  # data["sat"] = ((200 * (idx / lights.length.to_f)) + 54).round
+                  data["sat"] = target
+                  data["bri"] = target
                   with_transition_time(data, 0)
                 end
               end
