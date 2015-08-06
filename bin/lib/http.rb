a9f7f2773c3a63907a57d3187a4a104955e629f5
Jon Frisby <jfrisby@mrjoy.com>
2015-08-05 21:40:14 -0700

    Reduce allocations from calls to `Symbol#to_s`.

--- bin/lib/http.rb
+++ bin/lib/http.rb
@@ -5,10 +5,10 @@ require "oj"
 # TODO: Disable Curl from sending keepalives by trying HTTP/1.0.
 MULTI_OPTIONS   = { pipeline:         false,
                     max_connects:     (env_int("MAX_CONNECTS") || 3) }
-EASY_OPTIONS    = { timeout:          5,
-                    connect_timeout:  5,
-                    follow_location:  false,
-                    max_redirects:    0 }#,
+EASY_OPTIONS    = { "timeout" =>         5,
+                    "connect_timeout" => 5,
+                    "follow_location" => false,
+                    "max_redirects" =>   0 } #,
                     # version:          Curl::HTTP_1_0 }
 #   easy.header_str.grep(/keep-alive/)
 # Force keepalive off to see if that makes any difference...
