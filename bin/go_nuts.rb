7b2f452eff8d3b890691076c98512104da11fbf7
Jon Frisby <jfrisby@mrjoy.com>
2015-08-09 14:41:31 -0700

    Hoist this.

--- bin/go_nuts.rb
+++ bin/go_nuts.rb
@@ -23,6 +23,7 @@ Bundler.setup
 require "yaml"
 require "perlin_noise"
 
+require_relative "./lib/output"
 require_relative "./lib/config"
 require_relative "./lib/logging"
 require_relative "./lib/env"
@@ -306,38 +307,6 @@ threads = lights_for_threads.map do |(bridge_name, lights)|
       end
     end
   end
-end
-
-def format_float(num); num ? num.round(2) : "-"; end
-
-def format_rate(rate); "#{format_float(rate)}/sec"; end
-
-def print_stat(name, value, rate)
-  important "* #{value} #{name} (#{format_rate(rate)})"
-end
-
-STATS = [
-  ["requests",       :requests,      :requests_sec],
-  ["successes",      :successes,     :successes_sec],
-  ["failures",       :failures,      :failures_sec],
-  ["hard timeouts",  :hard_timeouts, :hard_timeouts_sec],
-  ["soft timeouts",  :soft_timeouts, :soft_timeouts_sec],
-]
-
-def print_stats(results)
-  STATS.each do |(name, count, rate)|
-    print_stat(name, results.send(count), results.send(rate))
-  end
-end
-
-# TODO: Show per-bridge and aggregate stats.
-def print_results(results)
-  important ""
-  print_stats(results)
-
-  important "* #{format_float(results.failure_rate)}% failure rate"
-  suffix  = " (#{format_float(results.elapsed / ITERATIONS.to_f)}/iteration)" if ITERATIONS > 0
-  important "* #{format_float(results.elapsed)} seconds elapsed#{suffix}"
 end
 
 # Wait for threads to finish initializing...
---
+++ bin/lib/output.rb
@@ -0,0 +1,31 @@
+def format_float(num); num ? num.round(2) : "-"; end
+
+def format_rate(rate); "#{format_float(rate)}/sec"; end
+
+def print_stat(name, value, rate)
+  important "* #{value} #{name} (#{format_rate(rate)})"
+end
+
+STATS = [
+  ["requests",       :requests,      :requests_sec],
+  ["successes",      :successes,     :successes_sec],
+  ["failures",       :failures,      :failures_sec],
+  ["hard timeouts",  :hard_timeouts, :hard_timeouts_sec],
+  ["soft timeouts",  :soft_timeouts, :soft_timeouts_sec],
+]
+
+def print_stats(results)
+  STATS.each do |(name, count, rate)|
+    print_stat(name, results.send(count), results.send(rate))
+  end
+end
+
+# TODO: Show per-bridge and aggregate stats.
+def print_results(results)
+  important ""
+  print_stats(results)
+
+  important "* #{format_float(results.failure_rate)}% failure rate"
+  suffix  = " (#{format_float(results.elapsed / ITERATIONS.to_f)}/iteration)" if ITERATIONS > 0
+  important "* #{format_float(results.elapsed)} seconds elapsed#{suffix}"
+end
