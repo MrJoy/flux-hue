88224f2dcd1913f80548c0937b0620154d7742da
Jon Frisby <jfrisby@mrjoy.com>
2015-08-09 17:11:51 -0700

    Uh...  I'm an idiot. >.<

--- bin/go_nuts.rb
+++ bin/go_nuts.rb
@@ -167,14 +167,11 @@ perlin_thread = Thread.new do
     Thread.stop
 
     loop do
-      prev = t = Time.now.to_f
-      loop do
-        t = Time.now.to_f
-        PERLIN.update(t)
-        elapsed = Time.now.to_f - t
-        # Try to adhere to a 10ms update frequency...
-        sleep FRAME_PERIOD - elapsed if elapsed < FRAME_PERIOD
-      end
+      t = Time.now.to_f
+      PERLIN.update(t)
+      elapsed = Time.now.to_f - t
+      # Try to adhere to a 10ms update frequency...
+      sleep FRAME_PERIOD - elapsed if elapsed < FRAME_PERIOD
     end
   end
 end
