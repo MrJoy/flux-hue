dd240481a5c15f2ded95746dbdc170f4a2ca5c1f
Jon Frisby <jfrisby@mrjoy.com>
2015-08-16 15:47:32 -0400

    We'll probably want several kinds of color objects...

--- bin/lib/color.rb
+++ bin/lib/color.rb
@@ -1,5 +1,5 @@
 # Helper functionality around colors.
-class Color
+module Color
   # TODO: Way of enforcing color limits...
   BLACK       = { r: 0,    g: 0,    b: 0    }.freeze
   DARK_GRAY   = { r: 0x0F, g: 0x0F, b: 0x0F }.freeze
