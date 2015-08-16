694c4ff2b710bdcfc087e580a5676fd55d5741f8
Jon Frisby <jfrisby@mrjoy.com>
2015-08-16 19:16:46 -0400

    Tweak colors for usability.

--- bin/color_scale.rb
+++ bin/color_scale.rb
@@ -1,18 +1,25 @@
 #!/usr/bin/env ruby
 require "rgb"
 
-def show_color(on)
+def to_hex(on)
   col = on.to_rgb.map { |n| ((n / 255.0) * 63.0).round.to_i }
   # puts "#{on.h.round(1)}, #{on.s.round(3)}, #{on.l.round(3)}"
-  puts "  0x%02X%02X%02X" % col
+  "0x%02X%02X%02X" % col
 end
 
-# on  = RGB::Color.from_rgb((0x27 / 63.0) * 255, (0x00 / 63.0) * 255, (0x3F / 63.0) * 255)
-on = RGB::Color.from_rgb((0x02 / 63.0) * 255, (0x00 / 63.0) * 255, (0x04 / 63.0) * 255)
+# on = RGB::Color.from_rgb((0x27 / 63.0) * 255, (0x00 / 63.0) * 255, (0x3F / 63.0) * 255)
+# on = RGB::Color.from_rgb((0x02 / 63.0) * 255, (0x00 / 63.0) * 255, (0x04 / 63.0) * 255)
+
+# Jen's palette color:
+# on = RGB::Color.from_fractions(49500/65535.0, 1.0, 0.5)
+on = RGB::Color.from_fractions(49500/65535.0, 1.0, 0.5)
 puts "#{on.h.round(1)}, #{on.s.round(3)}, #{on.l.round(3)}"
-show_color(on)
 
+results = []
+results << to_hex(on)
 5.times do
-  on.l -= 0.004
-  show_color(on)
+  on.l -= 0.075
+  results << to_hex(on)
 end
+
+puts "- #{results.reverse.join("\n- ")}"
