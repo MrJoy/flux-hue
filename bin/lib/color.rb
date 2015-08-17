1832a144c40320988af7ce364aedf9d029f3e22b
Jon Frisby <jfrisby@mrjoy.com>
2015-08-16 23:25:58 -0400

    Make dark gray darker.

--- bin/lib/color.rb
+++ bin/lib/color.rb
@@ -28,7 +28,7 @@ module Color
     end
 
     BLACK       = RGBColor.new(0x00, 0x00, 0x00).freeze
-    DARK_GRAY   = RGBColor.new(0x0F, 0x0F, 0x0F).freeze
+    DARK_GRAY   = RGBColor.new(0x07, 0x07, 0x07).freeze
     LIGHT_GRAY  = RGBColor.new(0x27, 0x27, 0x27).freeze
     WHITE       = RGBColor.new(0x3F, 0x3F, 0x3F).freeze
     RED         = RGBColor.new(0x3F, 0x00, 0x00).freeze
