cf09d58921d3cd785126ffd3ec35f3bae77a24b6
Jon Frisby <jfrisby@mrjoy.com>
2015-09-12 15:39:57 -0700

    Mark all lights, not just main lights.

--- bin/mark_lights.rb
+++ bin/mark_lights.rb
@@ -38,3 +38,53 @@ in_groups(CONFIG["main_lights"]).map do |(bridge_name, lights)|
   Curl::Multi.http(requests, MULTI_OPTIONS) do
   end
 end
+
+in_groups(CONFIG["dance_lights"]).map do |(bridge_name, lights)|
+  config    = CONFIG["bridges"][bridge_name]
+  lights    = lights.first.map(&:last)
+  counter   = 0
+  logger    = SparkleMotion.logger
+  requests  = lights
+              .map do |lid|
+                url = hue_light_endpoint(config, lid)
+                SparkleMotion::LazyRequestConfig.new(logger, config, url) do
+                  counter    += 1
+                  target      = (254 * (counter / lights.length.to_f)).round
+                  data        = {}
+                  data["on"]  = true
+                  data["hue"] = config["debug_hue"]
+                  # data["sat"] = ((200 * (idx / lights.length.to_f)) + 54).round
+                  data["sat"] = target
+                  data["bri"] = target
+                  with_transition_time(data, 0)
+                end
+              end
+
+  Curl::Multi.http(requests, MULTI_OPTIONS) do
+  end
+end
+
+in_groups(CONFIG["accent_lights"]).map do |(bridge_name, lights)|
+  config    = CONFIG["bridges"][bridge_name]
+  lights    = lights.first.map(&:last)
+  counter   = 0
+  logger    = SparkleMotion.logger
+  requests  = lights
+              .map do |lid|
+                url = hue_light_endpoint(config, lid)
+                SparkleMotion::LazyRequestConfig.new(logger, config, url) do
+                  counter    += 1
+                  target      = (254 * (counter / lights.length.to_f)).round
+                  data        = {}
+                  data["on"]  = true
+                  data["hue"] = config["debug_hue"]
+                  # data["sat"] = ((200 * (idx / lights.length.to_f)) + 54).round
+                  data["sat"] = target
+                  data["bri"] = target
+                  with_transition_time(data, 0)
+                end
+              end
+
+  Curl::Multi.http(requests, MULTI_OPTIONS) do
+  end
+end
