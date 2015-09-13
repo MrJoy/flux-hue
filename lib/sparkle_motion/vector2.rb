a4a731eaf4bc9b8c1357494652be164e7848664a
Jon Frisby <jfrisby@mrjoy.com>
2015-09-12 21:58:07 -0700

    Namespace another class.

--- lib/sparkle_motion/vector2.rb
+++ lib/sparkle_motion/vector2.rb
@@ -1,11 +1,12 @@
-# TODO: Namespacing!
-# A 2-component vector, where components go from 0.0..1.0.
-class Vector2
-  attr_reader :x, :y
-  def initialize(x: 0.0, y: 0.0)
-    @x = x
-    @y = y
-  end
+module SparkleMotion
+  # A 2-component vector, where components go from 0.0..1.0.
+  class Vector2
+    attr_reader :x, :y
+    def initialize(x: 0.0, y: 0.0)
+      @x = x
+      @y = y
+    end
 
-  def to_s; "<<#{@x}, #{@y}>>"; end
+    def to_s; "<<#{@x}, #{@y}>>"; end
+  end
 end
