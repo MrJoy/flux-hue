70c7024858d58f8704dc4e12776c49c585739194
Jon Frisby <jfrisby@mrjoy.com>
2015-09-17 16:10:27 -0700

    Fix: Properly evaluate status. >.<

--- tools/chunker.rb
+++ tools/chunker.rb
@@ -32,18 +32,18 @@ def organize_rest_result(data)
     else
       puts "WAT: #{result.inspect}"
     end
-    result_codes_distinct = result_codes.sort.uniq
-    if result_codes_distinct.length == 1
-      # Only one status.  Phew!
-      results["status"] = result_codes_distinct.first
-    else
-      # Not sure this outcome is actually *possible*, but the format
-      # of the response from the Hue Bridge seems to allow for it...
-      #
-      # You'll know which parameter(s) failed by the type of the value:
-      # If it's a `Hash`, there was an error.  Otherwise, it succeeded.
-      results["status"] = "mixed"
-    end
+  end
+  result_codes_distinct = result_codes.sort.uniq
+  if result_codes_distinct.length == 1
+    # Only one status.  Phew!
+    results["status"] = result_codes_distinct.first
+  else
+    # Not sure this outcome is actually *possible*, but the format
+    # of the response from the Hue Bridge seems to allow for it...
+    #
+    # You'll know which parameter(s) failed by the type of the value:
+    # If it's a `Hash`, there was an error.  Otherwise, it succeeded.
+    results["status"] = "mixed"
   end
   results
 end
