d0b04f29bdb9e7b92718ed022e823b4187095bd7
Jon Frisby <jfrisby@mrjoy.com>
2015-09-17 15:35:34 -0700

    Tidy/DRY up.

--- tools/chunker.rb
+++ tools/chunker.rb
@@ -62,6 +62,16 @@ def chunk(items, step_size = 0.1, digits = 1)
   chunks_out
 end
 
+def perform_with_timing(msg, &action)
+  printf "#{msg}..."
+  before = Time.now.to_f
+  action.call
+ensure
+  puts " #{(Time.now.to_f - before).round(2)} seconds."
+end
+
+def stringify_keys(hash); Hash[hash.map { |key, val| [key.to_s, val] }]; end
+
 lines       = []
 bucketed    = {}
 good_events = {}
@@ -70,78 +80,69 @@ all_events  = Set.new
 source      = ARGV.shift
 dest        = "#{source.sub(/\.raw\z/, '')}.yml"
 
-printf "Parsing raw data..."
-before = Time.now.to_f
-File.open(source, "r") do |f|
-  f.each_line do |line|
-    # TODO: Uh, we need proper CSV parsing here...  And proper CSV generation.
-    # TODO: In the meantime I'll cheat and rely on my knowledge that commas will
-    # TODO: only appear in the payload.
-    elts    = line.split(",", 4)
-    parsed  = { "time"    => elts[0].to_f,
-                "action"  => elts[1],
-                "url"     => elts[2],
-                "payload" => safe_parse(elts[3]) }
-    lines.push parsed
-    bucketed[parsed["url"]] ||= []
-    bucketed[parsed["url"]].push("time"    => parsed["time"],
-                                 "action"  => parsed["action"],
-                                 "payload" => parsed["payload"])
+perform_with_timing "Parsing raw data" do
+  File.open(source, "r") do |f|
+    f.each_line do |line|
+      # TODO: Uh, we need proper CSV parsing here...  And proper CSV generation.
+      # TODO: In the meantime I'll cheat and rely on my knowledge that commas will
+      # TODO: only appear in the payload.
+      elts    = line.split(",", 4)
+      parsed  = { "time"    => elts[0].to_f,
+                  "action"  => elts[1],
+                  "url"     => elts[2],
+                  "payload" => safe_parse(elts[3]) }
+      lines.push parsed
+      bucketed[parsed["url"]] ||= []
+      bucketed[parsed["url"]].push("time"    => parsed["time"],
+                                   "action"  => parsed["action"],
+                                   "payload" => parsed["payload"])
+    end
   end
 end
-puts " #{(Time.now.to_f - before).round(2)} seconds."
 
-printf "Organizing data..."
-before = Time.now.to_f
-bucketed.each do |url, events|
-  events.each_with_index do |event, index|
-    next unless event["action"] == "END" && index > 0 && events[index - 1]["action"] == "BEGIN"
-    raw       = url.split("/")
-    bridge    = raw[2]
-    light_id  = raw[6].to_i
-    # TODO: We'll need to pull config data to map this into a *logical* index!
-    light     = [bridge, light_id].join("-")
-    good_events[light] ||= []
-    good_events[light].push("start"         => events[index - 1]["time"],
-                            "duration"      => event["time"] - events[index - 1]["time"],
-                            "payload_begin" => events[index - 1]["payload"],
-                            "payload_end"   => organize_rest_result(event["payload"]))
-                            # light_id:      [bridge, light_id])
+perform_with_timing "Organizing data" do
+  bucketed.each do |url, events|
+    events.each_with_index do |event, index|
+      next unless event["action"] == "END" && index > 0 && events[index - 1]["action"] == "BEGIN"
+      raw       = url.split("/")
+      bridge    = raw[2]
+      light_id  = raw[6].to_i
+      # TODO: We'll need to pull config data to map this into a *logical* index!
+      light     = [bridge, light_id].join("-")
+      good_events[light] ||= []
+      good_events[light].push("start"         => events[index - 1]["time"],
+                              "duration"      => event["time"] - events[index - 1]["time"],
+                              "payload_begin" => events[index - 1]["payload"],
+                              "payload_end"   => organize_rest_result(event["payload"]))
+                              # light_id:      [bridge, light_id])
+    end
   end
 end
-puts " #{(Time.now.to_f - before).round(2)} seconds."
 
-printf "Extracting successful events..."
-before = Time.now.to_f
-good_events.each do |k, v|
-  v.sort_by! { |hsh| hsh["start"] }
-  chunked[k] = chunk(good_events[k])
-  all_events.merge chunked[k]
+perform_with_timing "Extracting successful events" do
+  good_events.each do |k, v|
+    v.sort_by! { |hsh| hsh["start"] }
+    chunked[k] = chunk(good_events[k])
+    all_events.merge chunked[k]
+  end
 end
-puts " #{(Time.now.to_f - before).round(2)} seconds."
 
 # sorted = all_events.sort_by { |hsh| hsh[:time] }
 
 # require "pry"
 # binding.pry
 
-def stringify_keys(hash); Hash[hash.map { |key, val| [key.to_s, val] }]; end
+simplified = perform_with_timing "Simplifying data for output" do
+  Hash[chunked.map { |idx, data| [idx, data.to_a] }]
+end
 
-printf "Simplifying data for output..."
-before = Time.now.to_f
-arrays_not_sets = chunked.map { |idx, data| [idx, data.to_a] }
-simplified      = arrays_not_sets
-puts " #{(Time.now.to_f - before).round(2)} seconds."
+output = perform_with_timing "Converting data to YAML" do
+  simplified.to_yaml
+end
 
-printf "Converting data to YAML..."
-before = Time.now.to_f
-output = Hash[simplified].to_yaml
-puts " #{(Time.now.to_f - before).round(2)} seconds."
-
-printf "Writing #{dest}..."
-before = Time.now.to_f
-File.write(dest, output)
-puts " #{(Time.now.to_f - before).round(2)} seconds."
+perform_with_timing "Writing #{dest}" do
+  File.write(dest, output)
+end
 
 # size_x = chunked.values.map(&:count).max
 # size_y = chunked.keys.last
