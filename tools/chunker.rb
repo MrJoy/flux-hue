ad3f72aa4408c9d8cb86743bbd8054831e7c0468
Jeremy Wagner-Kaiser <jeremy@cloudability.com>
2015-09-16 22:41:00 -0700

    Reorganize chunking a bit. Use Sets. About ready for PNG output.

--- tools/chunker.rb
+++ tools/chunker.rb
@@ -1,14 +1,17 @@
+require 'set'
+
 def chunk(items, step_size = 0.1, digits = 1)
-  chunks_out = {}
+  chunks_out = Set.new
   items.each do |item|
     start_bucket = item[:start].round(digits)
     duration = item[:duration].round(digits)
-    chunks_out[start_bucket] ||= []
+    # chunks_out[start_bucket] ||= []
     (start_bucket..start_bucket + duration).step(step_size) do |x|
       x               = x.round(digits)
-      chunks_out[x] ||= []
-      chunks_out[x].push(payload_begin: item[:payload_begin],
-                         payload_end:   item[:payload_end])
+      chunks_out.add( {payload_begin: item[:payload_begin],
+                         payload_end:   item[:payload_end],
+                         light_id:      item[:light_id],
+                         time:          x} )
     end
   end
   chunks_out
@@ -17,6 +20,8 @@ end
 lines       = []
 bucketed    = {}
 good_events = {}
+chunked     = {}
+all_events  = Set.new
 
 File.open("tmp/output.raw", "r") do |f|
   f.each_line do |line|
@@ -36,13 +41,28 @@ end
 bucketed.each do |url, events|
   events.each_with_index do |event, index|
     next unless event[:action] == "END" && index > 0 && events[index - 1][:action] == "BEGIN"
-    good_events[url] ||= []
-    good_events[url].push(start: events[index - 1][:time],
+    light_id = url.split('/')[6]
+    good_events[light_id.to_i] ||= []
+    good_events[light_id.to_i].push(start: events[index - 1][:time],
                           duration: event[:time] - events[index - 1][:time],
                           payload_begin: events[index - 1][:payload],
-                          payload_end: event[:payload])
+                          payload_end: event[:payload],
+                          light_id: light_id.to_i)
   end
 end
 
-# Use chunk on each element of the good_events hash.
-#  chunk good_events["http://192.168.2.10/api/1234567890/lights/35/state"]
+good_events.each do |k,v|
+  good_events[k].sort_by! { |hsh| hsh[:start] }
+  chunked[k] = chunk(good_events[k])
+  all_events.merge chunked[k]
+end
+
+sorted = all_events.sort_by { |hsh| hsh[:time]}
+
+require 'pry'
+binding.pry
+
+require 'chunky_png'
+png = ChunkyPNG::Image.new(chunked.values.map(&:count).max, chunked.keys.last, ChunkyPNG::Color::TRANSPARENT)
+
+# Getting pretty close now. Sorted contains all the data, ordered by timestamp.
