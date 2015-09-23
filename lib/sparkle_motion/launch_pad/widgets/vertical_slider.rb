810885eb35c3c8c53692ea4562a28381f521c254
Jon Frisby <jfrisby@mrjoy.com>
2015-09-23 02:35:20 -0700

    Snapshot of work.

--- lib/sparkle_motion/launch_pad/widgets/vertical_slider.rb
+++ lib/sparkle_motion/launch_pad/widgets/vertical_slider.rb
@@ -3,15 +3,18 @@ module SparkleMotion
     module Widgets
       # Class to represent a slider-style control on a Novation Launchpad.
       class VerticalSlider < Widget
-        attr_accessor :on_change
+        attr_accessor :on_change, :orbit
 
-        def initialize(launchpad:, position:, size:, colors:, on_change: nil, value: 0)
+        def initialize(launchpad:, position:, size:, colors:, on_change: nil, value: 0, orbit: nil)
           super(launchpad: launchpad,
                 position:  position,
                 size:      Vector2.new(1, size),
                 colors:    colors,
                 value:     value)
+          @orbit     = orbit
           @on_change = on_change
+
+          attach_orbit_handler!
         end
 
         def render
@@ -32,6 +35,19 @@ module SparkleMotion
           @value = y
           super(x: x, y: y)
           on_change.call(value) if on_change
+        end
+
+        def attach_orbit_handler!
+          return unless orbit
+          xx, yy = expand_range
+          orbit.response_to(:grid, :down, x: xx, y: yy) do |_inter, action|
+            handle_grid_response_down(action)
+            # TODO: Update Launchpad lights...
+          end
+          orbit.response_to(:grid, :up, x: xx, y: yy) do |_inter, action|
+            handle_grid_response_up(action)
+            # TODO: Update Launchpad lights...
+          end
         end
       end
     end
