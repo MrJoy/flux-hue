fa4b1c6c7d09ea3c35a720c2a504773610ee0423
Jon Frisby <jfrisby@mrjoy.com>
2015-09-25 15:51:18 -0700

    Notes to myself.

--- audio_test_fft.rb
+++ audio_test_fft.rb
@@ -9,8 +9,18 @@ require "fftw3"
 Thread.abort_on_exception = true
 
 WINDOW = 1024
+# TODO: Ask the device about it's frequency.
+# def bin_freq(idx); (idx * 44_100) / WINDOW; end
+def freq_bin(hz); ((hz * WINDOW) / 44_100.0).round; end
+
+# For WINDOW=1024, bin 7 == 301Hz, bin 70 == 3014Hz
+# F                 = 44100*x / WINDOW
+# F*WINDOW          = 44100*x
+# (F*WINDOW)/44_100 = x
 
 inbuf = CoreAudio.default_input_device.input_buffer(WINDOW)
+
+# TODO: Look into this to allow routing AudioHijack output into processor? http://www.ambrosiasw.com/utilities/wta/
 
 queue = Queue.new
 pitch_shift_th = Thread.start do
