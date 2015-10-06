e8b90cf3076a6ee44e82ee762853c76f43462f08
Jon Frisby <jfrisby@mrjoy.com>
2015-10-05 19:29:29 -0700

    Hoist.

---
+++ lib/sparkle_motion/simulation/render_task.rb
@@ -0,0 +1,41 @@
+module SparkleMotion
+  module Simulation
+    # Task to render to lights more or less as quickly as feasibl.
+    class RenderTask < SparkleMotion::ManagedTask
+      include SparkleMotion::Hue::HTTP
+
+      def initialize(node:, bridge:, lights:, transition:, global_results:, logger:, debug: false)
+        @node           = node
+        @bridge         = bridge
+        @lights         = lights
+        @transition     = transition
+        @data           = with_transition_time(@transition, "bri" => 0)
+        @global_results = global_results
+        @stats          = SparkleMotion::Results.new(logger: LOGGER) if @global_results
+        @debug          = debug
+        # TODO: Restore ITERATIONS functionality...
+        super("RenderTask[#{bridge['name']}]", :early, logger) { render }
+        @requests = @lights.map { |(idx, lid)| light_req(idx, lid) }
+      end
+
+      def render
+        Curl::Multi.http(@requests.dup, SparkleMotion::Hue::HTTP::MULTI_OPTIONS) do
+        end
+        return unless @global_results
+        @global_results.add_from(@stats)
+        @stats.clear!
+      end
+
+    protected
+
+      def light_req(idx, lid)
+        url = hue_light_endpoint(@bridge, lid)
+        SparkleMotion::Hue::LazyRequestConfig.new(@logger, @bridge, :put, url, @stats,
+                                                  debug: @debug) do
+          @data["bri"] = (@node[idx] * 255).round
+          @data
+        end
+      end
+    end
+  end
+end
