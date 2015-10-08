7c50da7701ffd470c45c9da263ee282e37b1009f
Jon Frisby <jfrisby@mrjoy.com>
2015-10-07 17:44:01 -0700

    DRY up.

--- lib/sparkle_motion/simulation/sweeper_task.rb
+++ lib/sparkle_motion/simulation/sweeper_task.rb
@@ -7,12 +7,18 @@ module SparkleMotion
       include SparkleMotion::Hue::HTTP
 
       def initialize(targets:, command_queue:, config:, logger:)
-        @targets        = targets
         @command_queue  = command_queue
         @hues           = config["values"]
         @transition     = config["transition"]
         @wait           = config["wait"]
         @data           = with_transition_time(@transition, "hue" => 0)
+        @targets        = targets
+                          .map do |(bridge, group_id)|
+                            url = group_update_url(bridge, group_id)
+                            SparkleMotion::Hue::LazyRequestConfig.new(logger, bridge, :put, url) do
+                              @data
+                            end
+                          end
 
         super("SweeperTask[#{config['name']}]", @wait, logger)
 
@@ -22,21 +28,13 @@ module SparkleMotion
       def tick(time)
         idx = ((time / @wait) % @hues.length).floor
         # TODO: Recycle hashes?
-        @targets.each do |(bridge, group_id)|
+        @targets.each do |req|
           @data["hue"] = @hues[idx]
-          add_group_command!(bridge, group_id, @data)
+          @command_queue << req
         end
       end
 
     protected
-
-      def add_group_command!(bridge, group_id, data)
-        return unless @command_queue
-        # TODO: Hoist the hash into something reusable?
-        @command_queue << { method:   :put,
-                            url:      group_update_url(bridge, group_id),
-                            put_data: Oj.dump(data) }.merge(SparkleMotion::Hue::HTTP::EASY_OPTIONS)
-      end
 
       def validate_params!
         logger.warn { "#{@name}: Wait should be >= transition!" } if @wait < @transition
