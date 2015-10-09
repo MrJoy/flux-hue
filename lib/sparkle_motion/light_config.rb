85d5a600a5f59b141d94df07aa90f378b2cc96d5
Jon Frisby <jfrisby@mrjoy.com>
2015-10-08 19:22:51 -0700

    Fix spotlighting behavior and make it easier to reason about this stuff.

--- lib/sparkle_motion/light_config.rb
+++ lib/sparkle_motion/light_config.rb
@@ -14,15 +14,14 @@ module SparkleMotion
         group_config  = config["light_groups"][group]
         num_lights   += group_config.length
 
-        group_lights_by_bridge(group_config).each do |bridge_name, lights|
+        group_config.each_with_index do |(bridge_name, light_id, kind), idx|
           lights_by_bridge[bridge_name] ||= []
-          lights_by_bridge[bridge_name] += lights
+          lights_by_bridge[bridge_name] << [idx, light_id, kind]
         end
       end
 
       lights_by_bridge.each do |(bridge_name, lights)|
-        lights_by_bridge[bridge_name], index = index_lights(lights, index)
-        masks_by_bridge[bridge_name] = mask_lights(lights_by_bridge[bridge_name], num_lights)
+        masks_by_bridge[bridge_name] = mask_lights(lights, num_lights)
       end
 
       set_state!(config, lights_by_bridge, masks_by_bridge)
@@ -34,25 +33,6 @@ module SparkleMotion
       @bridges  = config["bridges"]
       @lights   = lights
       @masks    = masks
-    end
-
-    def group_lights_by_bridge(config)
-      groups = Hash.new { |hsh, x| hsh[x] = [] }
-      config.each do |(bridge_name, light_id, kind)|
-        groups[bridge_name] << [light_id, kind]
-      end
-      groups
-    end
-
-    # Determine globally unique index for each light, so we can address them
-    # logically, but also relate physical mappings.
-    def index_lights(lights, index)
-      indexed_lights = []
-      lights.each do |(light_id, kind)|
-        indexed_lights << [index, light_id, kind]
-        index += 1
-      end
-      [indexed_lights, index]
     end
 
     # Create a mask for the global set of lights to easily tell which ones
