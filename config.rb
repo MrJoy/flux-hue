0a76d90a5ba550dc508fd82d6f49675acd882d76
Jon Frisby <jfrisby@mrjoy.com>
2016-04-27 09:01:32 -0700

    WIP

--- config.rb
+++ config.rb
@@ -1,23 +1,34 @@
+# TODO: Connect-4 clone.
+# TODO: Checkers clone.
+# TODO: AI for tic-tac-toe.
+
+# TODO: Count all diagonals.
+# TODO: Require contiguous lines!
+
 class TicTacToe
   OPEN_POSITION = { on: :dark_gray, off: :dark_gray, down: :white }
 
   PLAYERS = [{ on: :red,  off: :red,  down: :light_red },
              { on: :blue, off: :blue, down: :light_blue }]
 
+  BOARD_SIZE = 3
+  WIN_LENGTH = 3
+
   attr_accessor :grid
   attr_accessor :current_player
 
   def initialize
+    @max_coord = BOARD_SIZE - 1
     @gameover = false
-    @buttons  = (0..2).map { |_| (0..2).map { |_| nil } }
+    @buttons  = (0..@max_coord).map { |_| (0..@max_coord).map { |_| nil } }
     reset!
   end
 
   def positions
     @positions ||= begin
       tmp = []
-      (0..2).each do |x|
-        (0..2).each do |y|
+      (0..@max_coord).each do |x|
+        (0..@max_coord).each do |y|
           tmp << [x, y]
         end
       end
@@ -30,7 +41,7 @@ class TicTacToe
   end
 
   def reset!
-    @grid           = (0..2).map { |_| (0..2).map { |_| nil } }
+    @grid           = (0..@max_coord).map { |_| (0..@max_coord).map { |_| nil } }
     @current_player = 0
 
     @buttons.flatten.map do |widget|
@@ -44,7 +55,7 @@ class TicTacToe
   def handle_winner_in_line!(line, msg)
     winner =  line
               .group_by { |val| val }
-              .select { |_k, v| v.length == 3 }
+              .select { |_k, v| v.length == WIN_LENGTH }
               .values
               .flatten
               .first
@@ -59,9 +70,9 @@ class TicTacToe
 
     # Column-wise, row-wise, diagonals:
     lines = @grid +
-            (0..2).map { |row| @grid.map { |col| col[row] } } +
-            [(0..2).map { |x| @grid[x][x] },
-             (0..2).map { |x| @grid[x][2 - x] }]
+            (0..@max_coord).map { |row| @grid.map { |col| col[row] } } +
+            [(0..@max_coord).map { |x| @grid[x][x] },
+             (0..@max_coord).map { |x| @grid[x][@max_coord - x] }]
 
     lines.each_with_index do |col, idx|
       handle_winner_in_line!(col, "line ##{idx}")
